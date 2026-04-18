056114ca8b5f4972c7f3d71e5236997a17e4e5d6
Kevin <kevinchanchi02@gmail.com>
2026-04-18 15:18:26 -0500

    configuracion app Django inspections

---
+++ apps/inspections/apps.py
@@ -0,0 +1,17 @@
+from django.apps import AppConfig
+
+
+class InspectionsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.inspections'
+
+    _indexes_ready = False
+
+    def ready(self):
+        if InspectionsConfig._indexes_ready:
+            return
+
+        InspectionsConfig._indexes_ready = True
+        from apps.inspections.infrastructure.documents import Inspection
+
+        Inspection.ensure_indexes()
